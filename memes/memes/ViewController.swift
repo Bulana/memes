949670e243df033a94ca3036b09dbe0fff4be7a2
Bulana <Bulana@hal9000.zapper.local>
2019-04-22 14:22:09 +0200

    Project started, modelling reddit memes json

---
+++ memes/memes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memes
+//
+//  Created by Bulana on 2019/04/22.
+//  Copyright © 2019 Bulana. All rights reserved.
+//
+
+import UIKit
+
+//Model for reddit meme
+struct Meme {
+    let title: String
+    let subreddit: String
+    let author: String
+    let imageurl: String
+}
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
